e1a968f007ba4a557d7d04b7ea360a9935fd4c14
Stephen V <stephenrvasquez@gmail.com>
2024-06-13 00:56:44 -0500

    feat(ui): design and logic updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,15 @@ export enum Shape {
   Prism = 'prism',
   Cylinder = 'cylinder',
 }
+export enum Location {
+  Left = 'left',
+  Middle = 'middle',
+  Right = 'right',
+}
+export interface OutsideShape {
+  name: Shape | null;
+  composition: Shape[];
+}
 
 @Component({
   selector: 'app-root',
@@ -28,140 +37,295 @@ export enum Shape {
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  isSubmitted: boolean = false;
-  verityForm = new FormGroup({
-    insideLeft: new FormControl('', Validators.required),
-    insideMiddle: new FormControl('', Validators.required),
-    insideRight: new FormControl('', Validators.required),
-    outsideLeft: new FormControl('', Validators.required),
-    outsideMiddle: new FormControl('', Validators.required),
-    outsideRight: new FormControl('', Validators.required),
-  });
+  outsideShapes: OutsideShape[] = [
+    { name: Shape.Sphere, composition: [Shape.Circle, Shape.Circle] },
+    { name: Shape.Pyramid, composition: [Shape.Triangle, Shape.Triangle] },
+    { name: Shape.Cube, composition: [Shape.Square, Shape.Square] },
+    { name: Shape.Cone, composition: [Shape.Circle, Shape.Triangle] },
+    { name: Shape.Prism, composition: [Shape.Square, Shape.Triangle] },
+    { name: Shape.Cylinder, composition: [Shape.Circle, Shape.Square] },
+  ];
 
-  endingShapes: any = null;
-  dissectShapes: any = null;
+  insideLeft: Shape | null = null;
+  insideMiddle: Shape | null = null;
+  insideRight: Shape | null = null;
+  outsideLeft: OutsideShape | null = null;
+  outsideMiddle: OutsideShape | null = null;
+  outsideRight: OutsideShape | null = null;
+  startingSelections = [Shape.Circle, Shape.Square, Shape.Triangle];
+  startingLocations = [Location.Left, Location.Middle, Location.Right];
+  availableSelections = this.startingSelections;
+  availableLocations = this.startingLocations;
+  Location = Location;
+  Shape = Shape;
+  isSolvable: boolean = true;
+  stepByStepSolution: string[] = [];
+  solvedLeft: Shape[] = [];
+  solvedMiddle: Shape[] = [];
+  solvedRight: Shape[] = [];
+
+  // TODO: watch for when two selections have been made
+  makeSelection(selection: Shape, location: Location) {
+    this.availableSelections = this.availableSelections.filter(
+      (shape) => shape !== selection
+    );
+    this.availableLocations = this.availableLocations.filter(
+      (availableLocation) => availableLocation !== location
+    );
+    if (location === Location.Left) {
+      this.insideLeft = selection;
+    } else if (location === Location.Middle) {
+      this.insideMiddle = selection;
+    } else if (location === Location.Right) {
+      this.insideRight = selection;
+    }
+
+    if (this.availableLocations.length === 1) {
+      this.makeSelection(
+        this.availableSelections[0],
+        this.availableLocations[0]
+      );
+    }
+  }
+
+  selectOutside(selection: OutsideShape, location: Location) {
+    if (location === Location.Left) {
+      this.outsideLeft = JSON.parse(JSON.stringify(selection));
+    } else if (location === Location.Middle) {
+      this.outsideMiddle = JSON.parse(JSON.stringify(selection));
+    } else if (location === Location.Right) {
+      this.outsideRight = JSON.parse(JSON.stringify(selection));
+    }
+  }
 
   reset() {
-    this.isSubmitted = false;
-    this.endingShapes = null;
-    this.dissectShapes = null;
-    this.verityForm.reset();
+    this.isSolvable = true;
+    this.insideLeft = null;
+    this.insideMiddle = null;
+    this.insideRight = null;
+    this.outsideLeft = null;
+    this.outsideMiddle = null;
+    this.outsideRight = null;
+    this.availableSelections = this.startingSelections;
+    this.availableLocations = this.startingLocations;
+    this.stepByStepSolution = [];
+    this.solvedLeft = [];
+    this.solvedMiddle = [];
+    this.solvedRight = [];
   }
 
   doTheThing() {
-    this.isSubmitted = true;
-    this.verityForm.markAllAsTouched();
-    if (this.verityForm.valid) {
-      const input = this.verityForm.value;
-      const startingShapes = {
-        left: input.outsideLeft as Shape,
-        middle: input.outsideMiddle as Shape,
-        right: input.outsideRight as Shape,
-      };
-      this.endingShapes = {
-        left: this.findEndShape(input.insideLeft as Shape),
-        middle: this.findEndShape(input.insideMiddle as Shape),
-        right: this.findEndShape(input.insideRight as Shape),
-      };
-      this.dissectShapes = this.calcDissection(
-        startingShapes,
-        this.endingShapes
-      );
+    this.isSolvable = this.checkSolvable();
+    if (this.isSolvable) {
+      let startLeft = this.outsideLeft?.composition;
+      let startMiddle = this.outsideMiddle?.composition;
+      let startRight = this.outsideRight?.composition;
+      const endLeft = this.findEndShape(this.insideLeft)?.composition;
+      const endMiddle = this.findEndShape(this.insideMiddle)?.composition;
+      const endRight = this.findEndShape(this.insideRight)?.composition;
+
+      // Left
+      if (startLeft && endLeft) {
+        if (JSON.stringify(startLeft) === JSON.stringify(endLeft)) {
+          // left solved
+          this.stepByStepSolution.push('Left Complete!');
+          this.solvedLeft = startLeft || [];
+        } else {
+          let dissectShapes: Shape[] = [];
+          if (startLeft) {
+            // Check for dissections
+            if (startLeft[0] !== endLeft[0] && startLeft[0] !== endLeft[1]) {
+              dissectShapes.push(startLeft[0]);
+            } else {
+              this.solvedLeft.push(startLeft[0]);
+            }
+            if (
+              startLeft[0] === startLeft[1] ||
+              (startLeft[1] !== endLeft[0] && startLeft[1] !== endLeft[1])
+            ) {
+              dissectShapes.push(startLeft[1]);
+            } else {
+              this.solvedLeft.push(startLeft[1]);
+            }
+
+            // Find what's needed
+            let neededShapes = endLeft.filter(
+              (shape: Shape) =>
+                !dissectShapes.includes(shape) &&
+                !this.solvedLeft.includes(shape)
+            );
+
+            for (let i = 0; i < neededShapes.length; i++) {
+              // Look for shape in middle
+              if (startMiddle?.includes(neededShapes[i])) {
+                this.stepByStepSolution.push(
+                  `Dissect ${dissectShapes[i]} from Left`
+                );
+                this.stepByStepSolution.push(
+                  `Dissect ${neededShapes[i]} from Middle`
+                );
+                if (startMiddle[0] === neededShapes[i]) {
+                  startMiddle.splice(0, 1);
+                } else {
+                  startMiddle.splice(1, 1);
+                }
+                startMiddle.push(dissectShapes[i]);
+                dissectShapes.splice(i, 1);
+                this.solvedLeft.push(neededShapes[i]);
+              } else if (startRight?.includes(neededShapes[i])) {
+                // Look for shape on right
+                this.stepByStepSolution.push(
+                  `Dissect ${dissectShapes[i]} from Left`
+                );
+                this.stepByStepSolution.push(
+                  `Dissect ${neededShapes[i]} from Right`
+                );
+                if (startRight[0] === neededShapes[i]) {
+                  startRight.splice(0, 1);
+                } else {
+                  startRight.splice(1, 1);
+                }
+                startRight.push(dissectShapes[i]);
+                dissectShapes.splice(i, 1);
+                this.solvedLeft.push(neededShapes[i]);
+              }
+            }
+          }
+          this.stepByStepSolution.push('Left Complete!');
+        }
+      }
+
+      // Middle
+      if (startMiddle && endMiddle) {
+        if (JSON.stringify(startMiddle) === JSON.stringify(endMiddle)) {
+          // left solved
+          this.stepByStepSolution.push('Middle Complete!');
+          this.solvedMiddle = startMiddle || [];
+        } else {
+          let dissectShapes: Shape[] = [];
+          if (startMiddle) {
+            // Check for dissections
+            if (
+              startMiddle[0] !== endMiddle[0] &&
+              startMiddle[0] !== endMiddle[1]
+            ) {
+              dissectShapes.push(startMiddle[0]);
+            } else {
+              this.solvedMiddle.push(startMiddle[0]);
+            }
+            if (
+              startMiddle[0] === startMiddle[1] ||
+              (startMiddle[1] !== endMiddle[0] &&
+                startMiddle[1] !== endMiddle[1])
+            ) {
+              dissectShapes.push(startMiddle[1]);
+            } else {
+              this.solvedMiddle.push(startMiddle[1]);
+            }
+
+            // Find what's needed
+            let neededShapes = endMiddle.filter(
+              (shape) =>
+                !dissectShapes.includes(shape) &&
+                !this.solvedMiddle.includes(shape)
+            );
+
+            for (let i = 0; i < neededShapes.length; i++) {
+              if (startRight?.includes(neededShapes[i])) {
+                // Look for shape on right
+                this.stepByStepSolution.push(
+                  `Dissect ${dissectShapes[i]} from Middle`
+                );
+                this.stepByStepSolution.push(
+                  `Dissect ${neededShapes[i]} from Right`
+                );
+                if (startRight[0] === neededShapes[i]) {
+                  startRight.splice(0, 1);
+                } else {
+                  startRight.splice(1, 1);
+                }
+                startRight.push(dissectShapes[i]);
+                dissectShapes.splice(i, 1);
+                this.solvedMiddle.push(neededShapes[i]);
+              }
+            }
+          }
+          this.stepByStepSolution.push('Middle Complete!');
+        }
+      }
+
+      // Right
+      this.solvedRight = startRight || [];
+      this.stepByStepSolution.push('Right Complete!');
     }
   }
 
-  findEndShape(shape: Shape) {
+  checkSolvable() {
+    let circleCount = 0;
+    let squareCount = 0;
+    let triangleCount = 0;
+
+    if (this.outsideLeft) {
+      for (let i = 0; i < this.outsideLeft.composition.length; i++) {
+        const shape = this.outsideLeft.composition[i];
+        if (shape === Shape.Circle) {
+          circleCount++;
+        } else if (shape === Shape.Square) {
+          squareCount++;
+        } else if (shape === Shape.Triangle) {
+          triangleCount++;
+        }
+      }
+    }
+
+    if (this.outsideMiddle) {
+      for (let i = 0; i < this.outsideMiddle.composition.length; i++) {
+        const shape = this.outsideMiddle.composition[i];
+        if (shape === Shape.Circle) {
+          circleCount++;
+        } else if (shape === Shape.Square) {
+          squareCount++;
+        } else if (shape === Shape.Triangle) {
+          triangleCount++;
+        }
+      }
+    }
+
+    if (this.outsideRight) {
+      for (let i = 0; i < this.outsideRight.composition.length; i++) {
+        const shape = this.outsideRight.composition[i];
+        if (shape === Shape.Circle) {
+          circleCount++;
+        } else if (shape === Shape.Square) {
+          squareCount++;
+        } else if (shape === Shape.Triangle) {
+          triangleCount++;
+        }
+      }
+    }
+
+    return circleCount === 2 && squareCount === 2 && triangleCount === 2;
+  }
+
+  findEndShape(shape: Shape | null): OutsideShape | null {
     switch (shape) {
       case Shape.Circle:
-        return Shape.Prism;
+        return {
+          name: Shape.Prism,
+          composition: [Shape.Square, Shape.Triangle],
+        };
       case Shape.Square:
-        return Shape.Cone;
+        return {
+          name: Shape.Cone,
+          composition: [Shape.Circle, Shape.Triangle],
+        };
       case Shape.Triangle:
-        return Shape.Cylinder;
+        return {
+          name: Shape.Cylinder,
+          composition: [Shape.Circle, Shape.Square],
+        };
       default:
-        return '';
+        return null;
     }
-  }
-
-  calcDissection(startingShapes: any, endingShapes: any) {
-    const startingCodes = {
-      left: this.mapShapeToCode(startingShapes.left),
-      middle: this.mapShapeToCode(startingShapes.middle),
-      right: this.mapShapeToCode(startingShapes.right),
-    };
-    const endingCodes = {
-      left: this.mapShapeToCode(endingShapes.left),
-      middle: this.mapShapeToCode(endingShapes.middle),
-      right: this.mapShapeToCode(endingShapes.right),
-    };
-    let dissectLeft: Shape[] = [];
-    let dissectMiddle: Shape[] = [];
-    let dissectRight: Shape[] = [];
-
-    // Calc Left
-    if (startingShapes.left !== endingShapes.left) {
-      dissectLeft = this.calcDissectionShape(
-        startingCodes.left,
-        endingCodes.left
-      );
-    }
-
-    // Calc Middle
-    if (startingShapes.middle !== endingShapes.middle) {
-      dissectMiddle = this.calcDissectionShape(
-        startingCodes.middle,
-        endingCodes.middle
-      );
-    }
-
-    // Calc Right
-    if (startingShapes.right !== endingShapes.right) {
-      dissectRight = this.calcDissectionShape(
-        startingCodes.right,
-        endingCodes.right
-      );
-    }
-    return {
-      left: dissectLeft,
-      middle: dissectMiddle,
-      right: dissectRight,
-    };
-  }
-
-  mapShapeToCode(shape: Shape): Shape[] {
-    switch (shape) {
-      case Shape.Sphere:
-        return [Shape.Circle, Shape.Circle];
-      case Shape.Pyramid:
-        return [Shape.Triangle, Shape.Triangle];
-      case Shape.Cube:
-        return [Shape.Square, Shape.Square];
-      case Shape.Cone:
-        return [Shape.Circle, Shape.Triangle];
-      case Shape.Prism:
-        return [Shape.Square, Shape.Triangle];
-      case Shape.Cylinder:
-        return [Shape.Circle, Shape.Square];
-      default:
-        return [];
-    }
-  }
-
-  calcDissectionShape(startingCode: Shape[], endingCode: Shape[]): Shape[] {
-    let dissectionShapes: Shape[] = [];
-    if (
-      startingCode[0] !== endingCode[0] &&
-      startingCode[0] !== endingCode[1]
-    ) {
-      dissectionShapes.push(startingCode[0]);
-    }
-    if (
-      startingCode[1] !== endingCode[0] &&
-      startingCode[1] !== endingCode[1]
-    ) {
-      dissectionShapes.push(startingCode[1]);
-    }
-
-    return dissectionShapes;
   }
 }
