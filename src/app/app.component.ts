61aa21fdf6481d41a7b9374f76430448867c872d
Stephen V <stephenrvasquez@gmail.com>
2024-06-13 12:40:18 -0500

    chore: cleanup and change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 export enum Shape {
@@ -32,7 +26,7 @@ export interface OutsideShape {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
